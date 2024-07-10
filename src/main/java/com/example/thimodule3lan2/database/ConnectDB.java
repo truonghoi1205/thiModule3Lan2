0bca92c66efb40760617b60672db744407cd5783
truonghoi1205 <phamtruonghoi.dmx@gmail.com>
2024-07-10 08:56:18 +0700

    new repo

---
+++ src/main/java/com/example/thimodule3lan2/database/ConnectDB.java
@@ -0,0 +1,27 @@
+package com.example.thimodule3lan2.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+    private static String dbURL = "jdbc:mysql://localhost:3306/company_ABC?useSSL=false";
+    private static String dbUser = "root";
+    private static String dbPassword = "120599";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(dbURL, dbUser, dbPassword);
+            System.out.println("-----------------Kết nối thành công-----------------");
+        } catch (SQLException e) {
+            System.out.println("-----------------Lỗi kết nối-----------------");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("-----------------Lỗi kết nối abc-----------------");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
---
+++ src/main/webapp/views/list.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ASUS
+  Date: 7/10/2024
+  Time: 8:47 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+</head>
+<body>
+
+</body>
+</html>
